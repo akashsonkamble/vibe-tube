c8b98170014b7be5cff98536cc48681c50fa2bf8
akash <akashsonkamble199@gmail.com>
2024-05-20 19:37:15 +0530

    feat: add navbar component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,26 @@
+import { useState } from "react";
+import CategoryPills from "./components/CategoryPills";
+import { categories } from "./data/home";
+import PageHeader from "./layouts/PageHeader";
+
 const App = () => {
+    const [selectedCategory, setSelectedCategory] = useState(categories[0].id);
+    
     return (
         <div className="max-h-screen flex flex-col">
-            
+            <PageHeader />
+            <div className="grid grid-cols-[auto_1fr] flex-grow-1 overflow-auto">
+                <div>Sidebar</div>
+                <div className="overflow-x-hidden px-8 pb-4">
+                    <div className="sticky top-0 bg-white z-10 pb-4">
+                        <CategoryPills
+                            categories={categories}
+                            selectedCategory={selectedCategory}
+                            setSelectedCategory={setSelectedCategory} 
+                        />
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
---
+++ src/data/home.ts
@@ -0,0 +1,50 @@
+export const categories = [
+    {
+        id: 1,
+        name: "All",
+    },
+    {
+        id: 2,
+        name: "JavaScript",
+    },
+    {
+        id: 3,
+        name: "React",
+    },
+    {
+        id: 4,
+        name: "Typescript",
+    },
+    {
+        id: 5,
+        name: "Tailwind",
+    },
+    {
+        id: 6,
+        name: "Next.js",
+    },
+    {
+        id: 7,
+        name: "Programming",
+    },
+    {
+        id: 8,
+        name: "Frontend Development",
+    },
+    {
+        id: 9,
+        name: "Backend Development",
+    },
+    {
+        id: 10,
+        name: "Web Development",
+    },
+    {
+        id: 11,
+        name: "Mobile Development",
+    },
+    {
+        id: 12,
+        name: "Coding",
+    },
+]
